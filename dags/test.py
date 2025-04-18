30f35aa50be0506180366ffd96303515b035a389
Chidera Ozigbo <chideraozigbo@gmail.com>
2025-04-18 19:29:32 +0100

    made new update on the test dag

--- dags/test.py
+++ dags/test.py
@@ -9,6 +9,7 @@ default_args = {
     "depends_on_past": False,
     "email_on_failure": True,
     "email_on_retry": False,
+    "email_on_success": True,
     'email': ['chideraozigbo@gmail.com'],
     "retries": 1,
     "retry_delay": timedelta(seconds=3),
