debde32cc859adbf076bc62962749c1eec767b05
Chidera Ozigbo <chideraozigbo@gmail.com>
2025-04-21 05:13:37 +0100

    made changes to dag folder

--- dags/emr.py
+++ dags/emr.py
@@ -7,7 +7,7 @@ from airflow.providers.amazon.aws.operators.emr import (
 from airflow.providers.amazon.aws.sensors.emr import (EmrJobFlowSensor,
                                                       EmrStepSensor)
 from airflow.utils.trigger_rule import TriggerRule
-from notification.email import task_fail_alert
+from notification.email import task_state_alert
 
 DEFAULT_ARGS = {
     "owner": "team_4",
@@ -16,7 +16,8 @@ DEFAULT_ARGS = {
     "email_on_failure": True,
     "email_on_retry": False,
     "email_on_success": True,
-    'on_failure_callback': task_fail_alert,
+    'on_failure_callback': task_state_alert,
+    'on_success_callback': task_state_alert,
     "retries": 1,
     "retry_delay": timedelta(seconds=5),
 }
@@ -57,7 +58,7 @@ JOB_FLOW_OVERRIDES = {
 
 SPARK_STEPS = [
     {
-        "Name": "average_salary",
+        "Name": "test_run_spark_job",
         "ActionOnFailure": "CONTINUE",
         "HadoopJarStep": {
             "Jar": "command-runner.jar",
--- dags/notification/email.py
+++ dags/notification/email.py
@@ -5,11 +5,10 @@ from email.message import EmailMessage
 from airflow.models import Variable
 
 
-def task_fail_alert(context):
+def notification_email(context, state):
     """
     This function send notification of a failed dags task
     """
-    state = context.get("task_instance").state
     dag = context.get("task_instance").dag_id
     task = context.get("task_instance").task_id
     exec_date = context.get("task_instance").start_date
@@ -17,18 +16,21 @@ def task_fail_alert(context):
     dag_owner = context.get("task_instance").owner
     email_receiver = context.get("task_instance").email
 
+    if isinstance(email_receiver, list):
+        email_receiver = ", ".join(email_receiver)
+
     email_sender = Variable.get("email_sender")
     email_password = Variable.get("email_password")
     mail_server = Variable.get("MAIL_SERVER")
     email_port = int(Variable.get("email_port"))
 
     print(context)
-    subject = f"task {task} of {state} in dag {dag} failed"
+    subject = f"Airflow Alert: Task '{task}' in DAG '{dag}' has {state.upper()}"
     body = f"""
     Hey {dag_owner}
 
     The task {task} in dag {dag} runninng in Managed Apache Airflow \
-        has failed for run date {exec_date}
+        has {state} for run date {exec_date}
 
     Here is the log url: {log}
     """
@@ -38,9 +40,18 @@ def task_fail_alert(context):
     em["Subject"] = subject
     em.set_content(body)
 
-    context = ssl.create_default_context()
+    ssl_context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL(mail_server, email_port, context=context) as smtp:
+    with smtplib.SMTP_SSL(mail_server, email_port, context=ssl_context) as smtp:
         smtp.login(email_sender, email_password)
         smtp.sendmail(email_sender, email_receiver, em.as_string())
         print("Email Sent Successfully")
+
+
+def task_state_alert(context):
+    """
+    This function send notification of a dags task
+    """
+    state = context.get("task_instance").state
+    if state in ("success","failed"):
+        notification_email(context, state)
